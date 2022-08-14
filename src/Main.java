1c89e96ed3655fd5a90971253cd4bde954074224
VRR141 <v.r.ryazantsev@gmail.com>
2022-08-14 14:37:12 +0300

    2nd commit

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,19 @@ public class Main {
 
     public static void main(String[] args) {
         scanner = new Scanner(System.in);
-        String temp = scanner.nextLine();
-        System.out.println(calc(temp));
+        boolean flag = true;
+        System.out.println("ENTER OPERATION (format x + x), e for exit.");
+        while (true) {
+            String temp = scanner.nextLine();
+            if (temp.equalsIgnoreCase("E")) {
+                flag = false;
+                break;
+            }
+            System.out.println(calc(temp));
+        }
     }
 
-    public static String calc(String input){
+    public static String calc(String input) {
         Calculator calculator = new Calculator();
         return calculator.runCalculate(input);
     }
@@ -30,69 +38,68 @@ class Calculator {
             "XC", "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"
     };
 
-    public String runCalculate(String input){
+    public String runCalculate(String input) {
 
-            if (!containsArithmeticOperation(input)){
-                throw new RuntimeException("BAD ARITHMETIC OPERATION");
-            }
-
-            char[] chars = input.toCharArray();
-            int regex = 0;
-            char regexC = '.';
-
-            for (int i = 0; i < chars.length; i++){
-                if (chars[i] == '+' || chars[i] == '-' || chars[i] == '/' || chars[i] == '*'){
-                    regex = i;
-                    regexC = chars[i];
-                }
-            }
-
-            var chars1 = new char[regex];
-            var chars2 = new char[chars.length - regex - 1];
-            for (int i = 0; i < regex; i++){
-                chars1[i] = chars[i];
-            }
-            for (int i = 1 + regex; i < chars.length; i ++){
-                chars2[i - regex - 1] = chars[i];
-            }
-
-            String oneS = String.copyValueOf(chars1).trim();
-            String twoS = String.copyValueOf(chars2).trim();
-
-            boolean isRoman = false;
-
-            for (String s : roman) {
-                if (s.equals(oneS)) {
-                    isRoman = true;
-                    break;
-                }
-            }
-
-            String res = null;
-
-            if (isRoman) {
-                int one = convertRomanToNum(oneS);
-                int two = convertRomanToNum(twoS);
-                booms(one, two);
-                int result = calculate(one, two, regexC);
-                if (result < 1) {
-                    throw new RuntimeException("BAD ROMAN RESULT");
-                }
-                res = String.valueOf(convertNumToRoman(result));
-
-            }
-            else {
-                int one = Integer.parseInt(oneS);
-                int two = Integer.parseInt(twoS);
-                booms(one, two);
-                int result = calculate(one, two, regexC);
-                res = String.valueOf(result);
-            }
-            return res;
+        if (!containsArithmeticOperation(input)) {
+            throw new RuntimeException("BAD ARITHMETIC OPERATION");
         }
 
+        char[] chars = input.toCharArray();
+        int regex = 0;
+        char regexC = '.';
 
-    private boolean containsArithmeticOperation(String s){
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == '+' || chars[i] == '-' || chars[i] == '/' || chars[i] == '*') {
+                regex = i;
+                regexC = chars[i];
+            }
+        }
+
+        var chars1 = new char[regex];
+        var chars2 = new char[chars.length - regex - 1];
+        for (int i = 0; i < regex; i++) {
+            chars1[i] = chars[i];
+        }
+        for (int i = 1 + regex; i < chars.length; i++) {
+            chars2[i - regex - 1] = chars[i];
+        }
+
+        String oneS = String.copyValueOf(chars1).trim();
+        String twoS = String.copyValueOf(chars2).trim();
+
+        boolean isRoman = false;
+
+        for (String s : roman) {
+            if (s.equals(oneS)) {
+                isRoman = true;
+                break;
+            }
+        }
+
+        String res;
+
+        if (isRoman) {
+            int one = convertRomanToNum(oneS);
+            int two = convertRomanToNum(twoS);
+            booms(one, two);
+            int result = calculate(one, two, regexC);
+            if (result < 1) {
+                throw new RuntimeException("BAD ROMAN RESULT");
+            }
+            res = String.valueOf(convertNumToRoman(result));
+
+        } else {
+            int one = Integer.parseInt(oneS);
+            int two = Integer.parseInt(twoS);
+            booms(one, two);
+            int result = calculate(one, two, regexC);
+            res = String.valueOf(result);
+        }
+        return res;
+    }
+
+
+    private boolean containsArithmeticOperation(String s) {
         return s.contains("-") || s.contains("+") || s.contains("/") || s.contains("*");
     }
 
@@ -109,21 +116,21 @@ class Calculator {
         return result;
     }
 
-    private String convertNumToRoman (int numArabian) {
+    private String convertNumToRoman(int numArabian) {
         return roman[numArabian];
     }
 
-    private int convertRomanToNum (String romans) {
-        for (int i = 0; i < roman.length; i++){
-            if (romans.equals(roman[i])){
+    private int convertRomanToNum(String romans) {
+        for (int i = 0; i < roman.length; i++) {
+            if (romans.equals(roman[i])) {
                 return i;
             }
         }
         return -1;
     }
 
-    private void booms(int one, int two){
-        if ((one < 1 || one >10) || (two < 1 || two > 10)){
+    private void booms(int one, int two) {
+        if ((one < 1 || one > 10) || (two < 1 || two > 10)) {
             throw new RuntimeException("Incorrect input");
         }
     }
